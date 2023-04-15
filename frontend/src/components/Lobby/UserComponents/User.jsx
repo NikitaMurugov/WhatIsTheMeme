5426ab221176d267d4936945e1b43afed612865c
NikitaMurugov <idesignerdisk2@yandex.ru>
2023-04-16 01:53:27 +0500

    update(frontend): update lobby

--- frontend/src/components/Lobby/UserComponents/User.jsx
+++ frontend/src/components/Lobby/UserComponents/User.jsx
@@ -1,23 +1,27 @@
 import '../../../assets/css/user.css'
 import UserCards from "./UserCards";
+import { GiRoundStar } from "react-icons/gi";
 
 const User = ({user}) => {
     return <>
         <div className=" hover:scale-110 transition">
-            <div className="userCards flex gap-2 items-center mb-2 ">
+            <div className="userCards flex gap-2 items-center mb-6 ">
                 <UserCards cards={user.cards} />
             </div>
             <div className={'userContainer items-center '}>
                 <div className={'user'}>
-                    <div className={'user__head'}></div>
+                    <div className={'user__head'}>
+                    </div>
                     <div className={'user__body'} style={{
                         background: user.skinColor
                     }}></div>
                 </div>
+
                 {/*<hr className={`w-10/12 h-0.5 border-none bg-white opacity-10 rounded`}/>*/}
                 <div className={'username text-white'}>
                     {user.name}
                 </div>
+                {user.maintainer && <GiRoundStar color={"#fca32f"} width={'100%'} height={'100%'}/>}
             </div>
         </div>
     </>
--- frontend/src/pages/Lobby.jsx
+++ frontend/src/pages/Lobby.jsx
@@ -8,6 +8,7 @@ function Lobby() {
         {
             name: 'Антоним',
             skinColor: '#f7a352',
+            maintainer: false,
             cards: [
                 {
                     active: true,
@@ -30,6 +31,7 @@ function Lobby() {
         {
             name: 'Енакентий Третий',
             skinColor: '#52a6f7',
+            maintainer: false,
             cards: [
                 {
                     active: true,
@@ -52,6 +54,7 @@ function Lobby() {
         {
             name: 'Абдулахмед Самфаровдух',
             skinColor: '#96a47c',
+            maintainer: false,
             cards: [
                 {
                     active: true,
@@ -74,6 +77,30 @@ function Lobby() {
         {
             name: 'Маргарита ИЧОковна',
             skinColor: '#8a7ca4',
+            maintainer: false,
+            cards: [
+                {
+                    active: true,
+                    img: '',
+                },
+                {
+                    active: false,
+                    img: '',
+                },
+                {
+                    active: false,
+                    img: '',
+                },
+                {
+                    active: false,
+                    img: '',
+                },
+            ]
+        },
+        {
+            name: 'Вася пук',
+            skinColor: '#3aec97',
+            maintainer: true,
             cards: [
                 {
                     active: true,
